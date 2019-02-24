2b61de4aea76ed043cc5d8b92398298ce991a086
Alexey Sukhanov <yellocoma@gmail.com>
2019-02-24 23:23:35 +0300

    CHANGE: project directory strucrure

--- test/db.php
+++
@@ -1,4 +0,0 @@
-<?php
-$database_username  = 'root';
-$database_password  = '';
-$pdo_conn           = new PDO('mysql:host=localhost;dbname=med_ap1test', $database_username, $database_password);
--- test/delete.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-require_once("db.php");
-$pdo_statement = $DBH->prepare("DELETE FROM posts WHERE id=" . $_GET["id"]);
-$pdo_statement->execute();
-header("location: index.php");
