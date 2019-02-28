5a57e7a1a76c1ae255cd117a69e376c1455000f1
Alexey Sukhanov <yellocoma@gmail.com>
2019-02-28 22:41:34 +0300

    ADD: validate function + REF

--- core/Crud.php
+++ core/Crud.php
@@ -22,61 +22,57 @@ class Crud extends Database
 
     public function create()
     {
-        $trimed_doctor = trim($_POST["doctor"]);
-        $trimed_visit_date = trim($_POST["visit_date"]);
         $button_text = 'Записаться на приём';
-        if(!empty($trimed_doctor) && !empty($trimed_visit_date) && $this->date_is_correct($_POST["visit_date"])){
-            $sql = "INSERT INTO appointments ( doctor, visit_date, symptoms ) VALUES ( :doctor, :visit_date, :symptoms )";
-            $STH = $this->DBH->prepare( $sql );
-            $result = $STH->execute( array(":doctor"=>trim($_POST["doctor"]), ":visit_date" => trim($_POST["visit_date"]), ":symptoms" => trim($_POST["symptoms"])) );
-            if(!empty($result)) $this->read();
-        } elseif($_SERVER['REQUEST_METHOD'] == 'POST') {
-            $result["doctor"]       = trim($_POST["doctor"])  ?:'';
-            $result["visit_date"]   = trim($_POST["visit_date"]) ?:'';
-            $result["symptoms"]     = trim($_POST["symptoms"])     ?:'';
-            require_once  "views/form.php";
+        $result = $this->validate_values();
+        if($result["is_valid"]){
+            $STH = $this->DBH->prepare( "INSERT INTO appointments ( doctor, visit_date, symptoms ) VALUES ( :doctor, :visit_date, :symptoms )" );
+            $result = $STH->execute( array(":doctor"=>$result["doctor"], ":visit_date" => $result["visit_date"], ":symptoms" => $result["symptoms"]) );
+            if(!empty($result)) header('Location: http://'.$_SERVER['HTTP_HOST'] ."?action=read");
         } else {
-            require_once  "views/form.php";
+            include_once  "views/form.php";
         }
-    }
-
-    public function read()
-    {
-        $result = $this->DBH->query("SELECT * FROM appointments ORDER BY id DESC");
-        require_once "views/list.php";
     }
 
     public function update()
     {
-        $trimed_doctor = trim($_POST["doctor"]);
-        $trimed_visit_date = trim($_POST["visit_date"]);
         $button_text = 'Сохранить изменения';
-        if(!empty($trimed_doctor) && !empty($trimed_visit_date) && $this->date_is_correct($_POST["visit_date"])){
+        $result = $this->validate_values();
+        if($result["is_valid"]){
             $STH = $this->DBH->prepare("UPDATE appointments SET doctor=:doctor, visit_date=:visit_date, symptoms=:symptoms WHERE id=:id");
-            $result = $STH->execute(array(':doctor' => trim($_POST["doctor"]), ':visit_date' => trim($_POST["visit_date"]), ':symptoms' => trim($_POST["symptoms"]), ':id' => trim($_GET["id"])));
-            if(!empty($result)) $this->read();
-        } elseif ($_SERVER["REQUEST_METHOD"]  == 'POST') {
-            $result["doctor"]   = trim($_POST["doctor"])  ?:'';
-            $result["visit_date"]  = trim($_POST["visit_date"]) ?:'';
-            $result["symptoms"]      = trim($_POST["symptoms"])     ?:'';
-            require_once  "views/form.php";
-        } else {
+            $result = $STH->execute(array(':doctor' => $result["doctor"], ':visit_date' => $result["visit_date"], ':symptoms' => $result["symptoms"], ':id' => trim($_GET["id"])));
+            if(!empty($result)) header('Location: http://'.$_SERVER['HTTP_HOST'] ."?action=read");
+        } elseif ($_SERVER["REQUEST_METHOD"]  != 'POST') {
             $STH = $this->DBH->prepare("SELECT * FROM appointments WHERE id=?");
             $STH->execute(array($_GET["id"]));
             $result = $STH->fetchAll();
             $result = $result[0];
-            require_once  "views/form.php";
+            include_once  "views/form.php";
+        } else {
+            include_once  "views/form.php";
         }
+    }
+    public function read()
+    {
+        $result = $this->DBH->query("SELECT * FROM appointments ORDER BY id DESC");
+        include_once "views/list.php";
     }
 
     public function delete()
     {
-        $STH = $this->DBH->prepare("DELETE FROM appointments WHERE id=" . $_GET["id"]);
-        $STH->execute();
+        $STH = $this->DBH->prepare("DELETE FROM appointments WHERE id=?");
+        $STH->execute(array($_GET["id"]));
         $this->read();
     }
 
-    public function date_is_correct($date)
+    public function  validate_values(){
+        $result["doctor"]       = trim(htmlspecialchars(($_POST["doctor"])));
+        $result["visit_date"]   = trim(htmlspecialchars(($_POST["visit_date"])));
+        $result["symptoms"]     = trim(htmlspecialchars(($_POST["symptoms"])));
+        $result["is_valid"]     = !empty($result["doctor"]) && !empty($result["visit_date"]) && $this->validate_date($result["visit_date"]);
+        return $result;
+    }
+
+    public function validate_date($date)
     {
         $received_date_timestamp = strtotime(date_format( date_create($date), 'd-m-Y' ));
         $max_date__timestamp = time() + 60*60*24*5;
